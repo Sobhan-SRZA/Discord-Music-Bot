59ddb4d5d1c1d298d0d345156a2c83791e9bbe6c
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-10-28 23:03:59 +0330

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
 
         client.commands.forEach(cmd => {
             let cmdinfo = cmd.info
-            allcmds+="**"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"** ~ "+cmdinfo.description+"\n"
+            allcmds+="**"+process.env.PREFIX+cmdinfo.name+" "+cmdinfo.usage+"** ~ "+cmdinfo.description+"\n"
         })
 
         let embed = new MessageEmbed()
@@ -22,24 +22,21 @@ module.exports = {
         .setColor("RANDOME")
         .setThumbnail(message.client.user.displayAvatarURL({ format: "png" }))
         .setDescription(allcmds)
-        .setFooter(`To get info of each command you can do ${client.config.prefix}help | Create by Sobhan.SRZA#2153 :)`)
+        .setFooter(`To get info of each command you can do ${process.env.PREFIX}help | Create by Sobhan.SRZA#2153 :)`)
 
         if(!args[0])return message.channel.send(embed)
         else {
             let cmd = args[0]
             let command = client.commands.get(cmd)
             if(!command)command = client.commands.find(x => x.info.aliases.includes(cmd))
-            if(!command)return message.channel.send("Unknown Command")
+            if(!command)return message.channel.send("Nakhondam🤨")
             let commandinfo = new MessageEmbed()
             .setTitle("Command: "+command.info.name+" info")
             .setColor("RANDOM")
             .setDescription()
 .addField(
-        `**${client.config.prefix}${command.info.name}**`,
-        `${command.info.description} | Aliases: (${command.info.aliases.join(", ")}` : ""})`,
-        true
-      );
-           .addField(`**Links**`, `**[Support Server](${support_server || "https://discord.gg/4pUbjscCmA"}) • [Invite](https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=412353895745&scope=bot)**`)
+        `**${process.env.PREFIX}${command.info.name}**`,`${command.info.description( "")} | Aliases: (${command.info.aliases.join(", ")}` )
+           .addField(`**Links**`, `**[Support Server]("https://discord.gg/4pUbjscCmA"}) • [Invite](https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=412353895745&scope=bot)**`)
             message.channel.send(commandinfo)
         }
     }
