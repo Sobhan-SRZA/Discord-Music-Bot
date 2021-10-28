0b2af3054764ddfa56252f8f26cc8c555abac26b
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-10-28 22:35:30 +0330

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   await client.user.setActivity(`${PREFIX}help`, { type: "LISTENING" }) //can be LISTENING, WATCHING, PLAYING, STREAMING
-});
-};
+}
+
