6052518c51f7e1f8585ca281b3f0462cb30f9639
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-10-29 04:03:26 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//Loading .env
-require("dotenv").config();
+require("dotenv").config();//Loading .env
 const fs = require("fs");
 const { Collection, Client } = require("discord.js");
 
@@ -13,29 +12,25 @@ client.config = {
 
 //Loading Events
 fs.readdir(__dirname + "/events/", (err, files) => {
-      if (err) return console.error(err);
-
+  if (err) return console.error(err);
   files.forEach((file) => {
-    const event = require(__dirname +`/events/${file}`);
+    const event = require(__dirname + `/events/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
     console.log("Loading Event: "+eventName)
   });
-
 });
 
 //Loading Commands
-
-fs.readdir("/commands/", (err, files) => {
-        if (err) return console.error(err);
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
   files.forEach((file) => {
     if (!file.endsWith(".js")) return;
-    let props = require(`/commands/${file}`);
+    let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
     client.commands.set(commandName, props);
     console.log("Loading Command: "+commandName)
   });
-  
 },
         
 );
