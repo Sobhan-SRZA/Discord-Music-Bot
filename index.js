5a8ce56a5ec9b214f48d413cc93e97770915f7fe
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-12-29 01:25:12 +0330

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,24 @@ fs.readdir("./commands/", (err, files) => {
         
 );
 
+//status
+const srza = require('discord.js');
+srza.Constants.DefaultOptions.ws.properties.$browser = "Discord Android";
+client.on("ready", () => {
+   function YousamPower() {
+    let vazyiat = ["dnd","idle","online"] // online | dnd | idle | offline
+    let godrat = Math.floor(Math.random() * vazyiat.length)
+   client.user.setPresence({
+     status: vazyiat[godrat] })
+}; setInterval(YousamPower, 3000)
+   function srza() {
+    let sezar = [`${prefix}help`, `${prefix}play`,"Mr.SIN RE" , `🔰Sizar Team🔰`,`${client.guilds.cache.size} Servers` ]
+    let Power = Math.floor(Math.random() * sezar.length);
+    let statusPlay = ["LISTENING","WATCHING","PLAYING"] //can be LISTENING, WATCHING, PLAYING, STREAMING  
+    let godratPlay = Math.floor(Math.random() * statusPlay.length);     
+   client.user.setActivity(sezar[Power], {type: statusPlay[godratPlay]});
+        }; setInterval(srza, 3000)
+});
 
 //serverlist
 client.on('message', message => {
@@ -53,6 +71,34 @@ client.on('message', message => {
     if (!Guilds) return message.channel.send("No Guild");
     return message.channel.send(Guilds, { split: { char: "\n" } }); }
 });
+
+//bot stats
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content.startsWith(`${prefix}stats`)){
+    let infoEmbed = new Discord.MessageEmbed()
+      infoEmbed.setColor("RANDOM");
+      infoEmbed.setTitle(`Stats from \`${client.user.username}\``);
+      infoEmbed.addField(":ping_pong: Ping",`┕\`${Math.round(client.ws.ping)}ms\``,true);
+
+      infoEmbed.addField(":clock1: Uptime", `┕\`${moment.duration(message.client.uptime)}\``,true);
+      infoEmbed.addField(":file_cabinet: Memory",`┕\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
+            2
+          )}mb\``,true);
+
+      infoEmbed.addField(":homes: Servers",`┕\`${client.guilds.cache.size}\``, true);
+      infoEmbed.addField(":busts_in_silhouette: Users",`┕\`${client.users.cache.size}\``,true);
+      infoEmbed.addField(":control_knobs: API Latency",`┕\`${message.client.ws.ping}ms\``,true);
+      infoEmbed.addField(":robot: Version",`┕\`Alpha 1.0.0\``,true);
+
+      infoEmbed.addField(":blue_book: Discord.js",`┕\`v12.2.1\``,true);
+
+          infoEmbed.addField(":green_book: Node",`┕\`14.5.6\``,true);
+      infoEmbed.setTimestamp();
+      
+            message.channel.send(infoEmbed)
+    }
+    });
 
 //Logging in to discord
 client.login(process.env.TOKEN)
