c5ae20dac26bbffdb0ef9928a01331173a9f5971
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-02-09 14:14:15 +0330

    Delete ready.js

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
-  function YousamPower() {
-    let sezar = [`${process.env.PREFIX}play` , `${process.env.PREFIX}help` ]
-    let Power = Math.floor(Math.random() * sezar.length);
-    client.user.setActivity(sezar[Power], {type: "PLAYING"});//can be LISTENING, WATCHING, PLAYING, STREAMING
-  }; setInterval(YousamPower, 5000)
-    client.user.setStatus("dnd")//can be invesible, online, idle, dnd
-}
